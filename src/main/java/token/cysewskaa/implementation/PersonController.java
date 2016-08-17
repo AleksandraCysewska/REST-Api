78064f177820462bdcd4d7ba7afcb8193e5a8dab
AleksandraCysewska <aleksandra.cysewska@gmail.com>
2016-08-17 13:18:41 +0200

    dodano zagubione kontrollery

---
+++ src/main/java/token/cysewskaa/implementation/PersonController.java
@@ -0,0 +1,91 @@
+package token.cysewskaa.implementation;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import token.cysewskaa.crudInterfaces.DataToOrderDTO;
+import token.cysewskaa.crudInterfaces.OrderDTO;
+import token.cysewskaa.entities.AddressEntity;
+import token.cysewskaa.entities.PersonEntity;
+
+
+import javax.validation.Valid;
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * Created by izodorczyka on 2016-08-01.
+ */
+@RestController
+public class PersonController {
+
+    @Autowired
+    PersonRepo personRepo;
+
+    @Autowired
+    DataToOrderDTO dataToOrderDTO;
+
+@Autowired
+PersonEntityImpl personEntityImpl;
+    final static Logger logger = Logger.getLogger(PersonController.class);
+
+    @RequestMapping(value = "/person", method = RequestMethod.GET )
+    List<PersonEntity> personsGET(){
+
+       logger.info("POLICZONE " +  personRepo.count());
+        return personRepo.getAllPerson();
+    }
+
+    @RequestMapping(value = "/person/{personId}", method = RequestMethod.GET)
+    PersonEntity personByIdGET(@PathVariable("personId") long personId){
+
+
+        return personRepo.getPersonById(personId);
+                //personRepo.findOne(personId);
+    }
+
+
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(value = "/person", method = RequestMethod.POST, consumes = "application/json")
+    void personSET(@Valid @RequestBody PersonEntity data)
+    {
+personRepo.save(data);
+      //  personEntityImpl.setPerson(data);
+    }
+
+    @RequestMapping(value = "/person/{personId}", method = RequestMethod.PUT, consumes = "application/json")
+    void personPUT(@Valid @RequestBody PersonEntity data, @PathVariable("personId") long personId){
+
+        personEntityImpl.updatePerson(data,personId);
+    }
+    @RequestMapping(value = "address/{addressID}", method = RequestMethod.PUT, consumes = "application/json")
+    void addressPut(@Valid @RequestBody AddressEntity data, @PathVariable("addressID") long addressID){
+        PersonEntity one = personRepo.findOne(addressID);
+        one.getId();
+
+        personRepo.updateAddress(addressID, data);
+    }
+
+    @RequestMapping(value = "/person/{personId}", method = RequestMethod.DELETE)
+    void personDELETE(@PathVariable("personId") long personId){
+
+
+        personRepo.deletePersonById(personId);
+
+    }
+
+    @RequestMapping(value = "/dto/{dtoID}", method = RequestMethod.GET )
+    OrderDTO getDTO(@PathVariable("dtoID") long dtoID){
+        return   dataToOrderDTO.setFirstName(dtoID);
+    }
+
+
+}
---
+++ src/main/java/token/cysewskaa/implementation/PersonRepo.java
@@ -0,0 +1,46 @@
+package token.cysewskaa.implementation;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import token.cysewskaa.entities.AddressEntity;
+import token.cysewskaa.entities.PersonEntity;
+
+
+import java.util.List;
+
+/**
+ * Created by cysewskaa on 2016-08-10.
+ */
+
+@Transactional(readOnly = true)
+public interface PersonRepo extends JpaRepository<PersonEntity,Long> {
+
+    @Query("from token.cysewskaa.entities.PersonEntity ")
+     List<PersonEntity> getAllPerson();
+
+    @Query("select u from token.cysewskaa.entities.PersonEntity u where u.id=:id")
+    PersonEntity getPersonById(@Param("id") Long id);
+
+
+
+    @Modifying
+    @Transactional
+    @Query("delete from token.cysewskaa.entities.PersonEntity u where u.id=:id")
+    void deletePersonById(@Param("id") Long id);
+
+    @Modifying
+    @Query("update from token.cysewskaa.entities.PersonEntity u set u=:personEntity where u.id=:id")
+     void updatePerson(@Param("id") Long id, @Param("personEntity")PersonEntity personEntity);
+/*
+* TODO-FIX UPDATE*/
+
+    @Transactional
+    @Modifying
+    @Query("update from token.cysewskaa.entities.AddressEntity u set u=:addressEntity where u.id=:id")
+    void updateAddress(@Param("id") Long id, @Param("addressEntity")AddressEntity addressEntity);
+//
+}
